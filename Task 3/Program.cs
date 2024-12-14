8c4b115da1fea53dd5a131af345c8c3630fc84e6
Olaf Loewe <olafloewe95@gmail.com>
2024-12-14 03:43:07 +0100

    Task 3 complete

--- Task 3/Program.cs
+++ Task 3/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task_3 {
     internal class Program {
         static void Main(string[] args) {
+
+            String readLine;
+            Random rnd = new Random();
+
+            // loop till user breaks loop
+            while (true) {
+                // ask question
+                Console.Write("Do you want to print a random number between 0 and 100? input: (Y/N) ");
+
+                // get user answer and re ask on invalid input
+                readLine = Console.ReadLine();
+                if (readLine.ToLower() != "y" && readLine.ToLower() != "n") continue;
+
+                // user breaks loop
+                if(readLine.ToLower() == "n") break;
+
+                Console.WriteLine(rnd.Next(0,100));
+            }
         }
     }
 }
